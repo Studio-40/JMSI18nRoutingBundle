28bcdf1febbeb317379a03128ec1a508aed5dba8
Alan Pearce <ap@studio-40.com>
2014-11-20 10:28:25 +0000

    Don't override the default router
    In order to use the DynamicRouter from the CMF bundle, replace
    router.default in cmf_routing.chain.routers_by_id with
    jms_i18n_routing.router. This way, both internationalised routes and
    dynamic routes will work.
    
    Of course, the way in which this happens means that there's no
    localisation on dynamic routes, which may or may not work in every case.

--- DependencyInjection/Compiler/SetRouterPass.php
+++ DependencyInjection/Compiler/SetRouterPass.php
@@ -30,8 +30,6 @@ class SetRouterPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        $container->setAlias('router', 'jms_i18n_routing.router');
-
         $translatorDef = $container->findDefinition('translator');
         if ('%translator.identity.class%' === $translatorDef->getClass()) {
             throw new \RuntimeException('The JMSI18nRoutingBundle requires Symfony2\'s translator to be enabled. Please make sure to un-comment the respective section in the framework config.');
